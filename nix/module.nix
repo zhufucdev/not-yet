839cff5e30e2d934732448cf5a81eff6955b2e94
zhufucdev <zhufuzhufu1@gmail.com>
2026-05-30 13:56:34 +0800

    fix: runtime directory mode

--- nix/module.nix
+++ nix/module.nix
@@ -90,7 +90,7 @@ in
             ++ lib.optional (cfg.extraEnv != null) cfg.extraEnv;
           DynamicUser = true;
           RuntimeDirectory = [ "not-yet" ];
-          RuntimeDirectoryMode = "0666";
+          RuntimeDirectoryMode = "0754";
           WorkingDirectory = cfg.dataPath;
           StateDirectory = [ (lib.removePrefix "/var/lib/" cfg.dataPath) ];
           ReadWritePaths = [ cfg.dataPath ];
